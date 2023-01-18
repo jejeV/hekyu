92d75280a29e36e54a4075716d2e1c15de4cabbc
jejeV <muejason27@gmail.com>
2023-01-18 18:18:16 +0700

    view home

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import "./about.css";
+import Image from "../../assets/avatar-2.svg"
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className="about container section" id='about'>
+     <h2 className="section__title">About Me</h2>
+
+     <div className="about__container grid">
+       <img src={Image} alt="" className="img__about" />
+
+       <div className="about__data grid">
+        <div className="about__info">
+          <p className="about__description">I am Samuel Jason Rain, web
+          developer from Depok, Indonesia. I love coding and design.</p>
+          <a href="" className="btn">Download CV</a>
+        </div>
+
+        <div className="about__skills grid"></div>
+       </div>
+     </div>
+    </section>
   )
 }
 
---
+++ src/components/home/HeaderSocials.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const HeaderSocials = () => {
+  return (
+    <div className="home__socials">
+        <a href="#home" className="home__social-link" target="_blank">
+        <i class="fa-brands fa-instagram"></i>
+        </a>
+
+        <a href="#home" className="home__social-link" target="_blank">
+        <i class="fa-brands fa-twitter"></i>
+        </a>
+
+        <a href="#home" className="home__social-link" target="_blank">
+        <i class="fa-brands fa-github"></i>
+        </a>
+
+        <a href="#home" className="home__social-link" target="_blank">
+        <i class="fa-brands fa-linkedin"></i>
+        </a>
+
+        <a href="#home" className="home__social-link" target="_blank">
+        <i class="fa-brands fa-discord"></i>
+        </a>
+    </div>
+  )
+}
+
+export default HeaderSocials
