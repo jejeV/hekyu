c57177ec3c8d41f4b464840d5933b5841a2c513a
jejeV <muejason27@gmail.com>
2023-04-13 13:38:00 +0700

    contact

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ import About from './components/about/About';
 import Services from './components/services/Services';
 import Resume from './components/resume/Resume';
 import Portofolio from './components/portofolio/Portofolio';
-import Pricing from './components/pricing/Pricing';
-import Testimonials from './components/testimonials/Testimonials';
-import Blog from './components/blog/Blog';
+// import Pricing from './components/pricing/Pricing';
+// import Testimonials from './components/testimonials/Testimonials';
+// import Blog from './components/blog/Blog';
 import Contact from './components/contact/Contact';
 
 function App() {
@@ -21,9 +21,9 @@ function App() {
       <Services />
       <Resume />
       <Portofolio />
-      <Pricing />
+      {/* <Pricing />
       <Testimonials />
-      <Blog />
+      <Blog /> */}
       <Contact />
     </main>
     </>
